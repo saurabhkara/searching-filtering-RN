48293e3532d808d7b70be661af6c6bbc51b42374
Saurabh <saurabkumar679@gmail..com>
2023-04-25 06:35:08 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , FlatList} from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+
+  const [isLoading, setIsLOading] = useState(false);
+  const [isError, setIsError] = useState('');
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +24,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
